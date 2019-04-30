31e41cffa067aa0e7f58f0c7baa36118a6e49b7e
Suva Shahria <ss2479@null.cs.rutgers.edu>
2019-04-30 08:56:55 -0400

    proj3

---
+++ ll2/attr.h
@@ -0,0 +1,14 @@
+/**********************************************
+        CS415  Compilers  Example
+**********************************************/
+
+#ifndef ATTR_H
+#define ATTR_H
+
+typedef union {char* str; int num;} tokentype;
+
+typedef struct {  
+        int mcount;
+        } multCounter;
+
+#endif
